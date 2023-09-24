5c020898603eac47bea99ae191bc52b36238a12d
Jess Tada <jesstada@gmail.com>
2023-09-24 02:24:56 -0400

    once per day for real

--- pages/api/serverless-example/handler.ts
+++ pages/api/serverless-example/handler.ts
@@ -4,7 +4,7 @@ export default function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
-    
+    // Add code here
   response.status(200).json({
     body: request.body,
     query: request.query,

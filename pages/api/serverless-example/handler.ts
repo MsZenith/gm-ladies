54f2f5a02ff4c6e8ea2104875a15bf7763704221
Jess Tada <jesstada@gmail.com>
2023-09-24 02:21:54 -0400

    line

--- pages/api/serverless-example/handler.ts
+++ pages/api/serverless-example/handler.ts
@@ -4,6 +4,7 @@ export default function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
+    
   response.status(200).json({
     body: request.body,
     query: request.query,

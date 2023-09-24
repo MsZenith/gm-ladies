7957c0b46a97d36f60289b76409a973503cb9bcb
Jess Tada <jesstada@gmail.com>
2023-09-24 03:52:58 -0400

    update logo

--- pages/api/serverless-example/handler.ts
+++ pages/api/serverless-example/handler.ts
@@ -1,10 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { sendNotification } from "../../../utils/fetchNotify";
  
 export default function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
-    // Add code here
+    // Add code here to loop through all accounts and send notification to each of them!
+    // sendNotification({
+    //     // title: "GM Ladiesssss",
+    //     title: `Hello ${selectedHoroscope}, today ${HoroscopeData[selectedHoroscope]}`,
+    //     body: `${horoscopePrediction}`,
+    //     icon: `${window.location.origin}/WalletConnect-blue.svg`,
+    //     url: window.location.origin,
+    //     type: "promotional",
+    //   });
   response.status(200).json({
     body: request.body,
     query: request.query,
